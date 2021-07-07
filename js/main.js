c70871c730db745f76483b244adb805cbdc5345e
Fur1ouS <furiticus@gmail.com>
2021-07-07 17:41:28 +0300

    first push - add html and css

---
+++ js/main.js
@@ -0,0 +1,58 @@
+let menu = document.querySelector('.menu');
+
+menu.addEventListener('click', ({target}) => {
+    let targetAttr = target.dataset.btn;
+
+    switch (targetAttr) {
+        case 'books':
+            bookHidden(targetAttr);
+            return;
+        case 'visitors':
+            bookHidden(targetAttr);
+            return;
+        case 'cards':
+            bookHidden(targetAttr);
+            return;
+        case 'statistics':
+            bookHidden(targetAttr);
+            return;
+    }
+});
+
+let books = document.getElementById('books');
+let visitors = document.getElementById('visitors');
+let cards = document.getElementById('cards');
+let statistics = document.getElementById('statistics');
+
+function bookHidden(targetAttr){
+    if(targetAttr==='books'){
+        books.hidden = false;
+        visitors.hidden=true;
+        cards.hidden=true;
+        statistics.hidden=true;
+    }
+
+    if(targetAttr==='visitors'){
+        books.hidden = true;
+        visitors.hidden=false;
+        cards.hidden=true;
+        statistics.hidden=true;
+    }
+
+    if(targetAttr==='cards'){
+        books.hidden = true;
+        visitors.hidden=true;
+        cards.hidden=false;
+        statistics.hidden=true;
+    }
+
+    if(targetAttr==='statistics'){
+        books.hidden = true;
+        visitors.hidden=true;
+        cards.hidden=true;
+        statistics.hidden=false;
+    }
+
+}
+
+
