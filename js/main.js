48911edf4a1001accaae37aa7f8be535acd438df
Fur1ouS <furiticus@gmail.com>
2021-07-13 15:11:19 +0300

    function show array of books on page for load

--- js/main.js
+++ js/main.js
@@ -37,9 +37,14 @@ let books;
 
 window.addEventListener('DOMContentLoaded', () => {
     books = new Books();
-
-    console.log(books);
+    showBook(books);
 });
+
+let tbodyBooks = document.getElementById('table-books').tBodies[0];
+function showBook(books){
+    let book = JSON.parse(localStorage.getItem('books')) || [];
+    console.log(book);
+}
 
 window.addEventListener('unload', () => {
     books?.setToLocalStorage();
